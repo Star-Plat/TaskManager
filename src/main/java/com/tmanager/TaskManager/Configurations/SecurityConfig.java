270812d790dd9daff0683269b81ddce46e9cf55f
Shuv <shuvib2@gmail.com>
2025-06-14 09:41:12 +0530

    Edited security config eith cors for vercel

--- src/main/java/com/tmanager/TaskManager/Configurations/SecurityConfig.java
+++ src/main/java/com/tmanager/TaskManager/Configurations/SecurityConfig.java
@@ -74,7 +74,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://task-manager-iota-taupe.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
