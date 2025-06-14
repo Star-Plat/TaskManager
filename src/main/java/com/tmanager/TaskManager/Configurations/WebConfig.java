a458a30b46fd9e29e8c5b07fb50a5a5388836440
Shuv <shuvib2@gmail.com>
2025-06-14 06:32:21 +0530

    Web config final

--- src/main/java/com/tmanager/TaskManager/Configurations/WebConfig.java
+++ src/main/java/com/tmanager/TaskManager/Configurations/WebConfig.java
@@ -14,8 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("https://task-manager-iota-taupe.vercel.app")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://task-manager-iota-taupe.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
